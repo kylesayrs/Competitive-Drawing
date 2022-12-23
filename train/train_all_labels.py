7b45f0baa93906406290dd65f65e754f771d609e
Kyle Sayers <kylesayrs@gmail.com>
2022-12-23 04:32:24 +0000

    stably training

---
+++ train/train_all_labels.py
@@ -0,0 +1,33 @@
+import os
+import boto3
+import random
+import itertools
+
+from helpers import get_all_local_labels
+from train_model import train_model
+
+
+S3_CLIENT = boto3.client("s3")
+
+
+if __name__ == "__main__":
+    all_local_labels = get_all_local_labels()
+
+    print(f"Training {all_local_labels}")
+
+    train_model(
+        all_local_labels,
+        num_epochs=50,
+        batch_size=256,
+        test_batch_size=256,
+        lr=0.01,
+        momentum=None,
+        optimizer="Adam",
+        cutmix_prob=0.0,
+        logging_rate=1000,
+        patience_length=None,
+        patience_threshold=None,
+        model_name="Megamodel",
+    )
+
+    print("Done training all models")
