366d12197105d443ba55abd056c7b2f8ff7d4bcc
Kyle Sayers <kylesayrs@gmail.com>
2022-12-25 01:50:06 -0500

    Begin generalizing for curves

--- custom_modules/render.py
+++ custom_modules/render.py
@@ -1,107 +1,170 @@
+from typing import List, Optional
+
 import cv2
 import torch
 
 
-class LineRaster2d(torch.nn.Module):
+class PathRaster2d(torch.nn.Module):
     """
-    Renders a line from p0 to p1 on the canvas.
-    The result is differentiable at all points
-    with respect to the inputs (although we restrict
-    rendering based on line_width)
-
-    len_of_projection = torch.dot(p - p0, p1 - p0) / torch.norm(p1 - p0)
-    t = len_of_projection / torch.norm(p1 - p0)  |  0 ≤ t ≤ 1
-    projection = p0 + t * (p1 - p0)
-    distance_to_line = torch.norm(p - projection)
     """
 
-    def __init__(self, canvas_shape, line_width=1.0):
+    def __init__(
+        self,
+        canvas_shape: List[int],
+        line_width: float = 1.0,
+        anti_aliasing: str = "linear",
+    ):
         super().__init__()
         self.canvas_shape = canvas_shape
         self.line_width = line_width
+        self.anti_aliasing = anti_aliasing
 
-    def forward(self, p0, p1):
-        p0 = p0 * torch.tensor(self.canvas_shape)
-        p1 = p1 * torch.tensor(self.canvas_shape)
+        self.max_distance = torch.norm(
+            torch.tensor(canvas_shape) - torch.tensor([0.0, 0.0])
+        )
+
+        if self.anti_aliasing not in ["linear", "quadratic", "global"]:
+            raise ValueError("anti_aliasing must be 'local', 'quadratic' or 'global'")
+
+
+    def get_distance_to_line(
+        self,
+        p: torch.tensor,
+        p0: torch.tensor,
+        p1: torch.tensor,
+        p1_minus_p0: Optional[float] = None,
+        len_line_segment_squared: Optional[float] = None
+    ):
+        """
+        This one has a closed form solution
+        """
+        p1_minus_p0 = p1 - p0 if p1_minus_p0 is None else p1_minus_p0
+        len_line_segment_squared = (
+            (p1[0] - p0[0]) ** 2 + (p1[1] - p0[1]) ** 2
+            if len_line_segment_squared is None
+            else len_line_segment_squared
+        )
+
+        t_guess = torch.dot(p - p0, p1_minus_p0) / len_line_segment_squared
+        t = torch.clamp(t_guess, min=0, max=1)
+        projection = p0 + t * p1_minus_p0
+
+        return torch.norm(p - projection)
+
+
+    def sample_distances_on_path(self, key_points: List[torch. tensor], t: float):
+        if len(key_points) == 1:
+            return key_points[0]
+
+        if len(key_points) == 2:
+            return key_points[0]
+
+        else:
+            raise ValueError(f"Invalid number of key points, found {len(key_points)}")
+
+
+    def forward(self, key_points):
+        # prepare canvas and keypoints
         canvas = torch.zeros(self.canvas_shape)
+        canvas_shape_tensor = torch.tensor(self.canvas_shape)
+        key_points = [key_point * canvas_shape_tensor for key_point in key_points]
 
-        # precompute some values
-        p1_minus_p0 = p1 - p0
-        len_line_segment_squared = (p1[0] - p0[0]) ** 2 + (p1[1] - p0[1]) ** 2
+        # precompute some values if applicable
+        if len(key_points) == 2:
+            p1_minus_p0 = key_points[1] - key_points[0]
+            len_line_segment_squared = (
+                (key_points[1][0] - key_points[0][0]) ** 2 +
+                (key_points[1][1] - key_points[0][1]) ** 2
+            )
 
         # torch doesn't implement a map function, so a single thread will do
         for y in range(0, canvas.shape[0]):
             for x in range(0, canvas.shape[1]):
-                p = torch.tensor([x, y], dtype=torch.float32)
+                p = torch.tensor([y, x], dtype=torch.float32)
 
-                p_minus_p0 = p - p0
-                t_guess = torch.dot(p_minus_p0, p1_minus_p0) / len_line_segment_squared
-                t = torch.clamp(t_guess, min=0, max=1)
-                projection = p0 + t * p1_minus_p0
+                if len(key_points) == 2:
+                    distance = self.get_distance_to_line(
+                        p,
+                        *key_points,
+                        p1_minus_p0=p1_minus_p0,
+                        len_line_segment_squared=len_line_segment_squared
+                    )
 
-                distance = torch.norm(p - projection)
-                if distance < self.line_width:
-                    canvas[x, y] = self.line_width - distance
+                if self.anti_aliasing == "linear" and distance < self.line_width:
+                    canvas[y, x] = self.line_width - distance
+
+                if self.anti_aliasing == "quadratic":
+                    canvas[y, x] = 1 - (distance / self.max_distance) ** (1 / 3)
+
+                if self.anti_aliasing == "global":
+                    canvas[y, x] = distance / self.max_distance
 
         return canvas
 
 
 class OpponentModel(torch.nn.Module):
+    """
+    Given a canvas, score model, and maximum path length,
+    this model finds an optimal path that maximizes the
+    score with the given contraints
+
+    The first technique places the path at random positions
+    on the canvas, optimizes each, then picks the optimized
+    path with the highest score
+
+    The second technique starts with a path with a large
+    aliasing width, then slowly decreases that width as the
+    path is optimized, converging on a global solution
+    """
+
     def __init__(self, canvas_shape, line_width=2.0):
         super(OpponentModel, self).__init__()
 
-        # In the future these will be computed outputs from a
-        # model that takes the original canvas as input
-        self.p0 = torch.nn.Parameter(torch.rand(2), requires_grad=True)
-        self.p1 = torch.nn.Parameter(torch.rand(2), requires_grad=True)
+        # key points to optimize
+        self.key_points = [
+            torch.nn.Parameter(torch.rand(2), requires_grad=True)
+            for _ in range(2)
+        ]
 
         # line rendering
-        self.line_raster = LineRaster2d(canvas_shape, line_width=2.0)
+        self.line_raster = PathRaster2d(canvas_shape, line_width=2.0)
 
         # In the future we'll feed the render to the original
         # classifier to compute our score
 
+    def parameters(self):
+        """
+        Future: do this properly, something like making the parameters a module?
+        """
+        model_parameters = super(OpponentModel, self).parameters()
+        return list(model_parameters) + self.key_points
+
     def forward(self):
-        p0 = torch.sigmoid(self.p0 * 1)
-        p1 = torch.sigmoid(self.p1 * 1)
-        output_canvas = self.line_raster(p0, p1)
+        key_points = [
+            torch.sigmoid(key_point * 1)
+            for key_point in self.key_points
+        ]
+        print(key_points)
+
+        output_canvas = self.line_raster(self.key_points)
 
         return output_canvas
-
-
-# Notes: There are many contraints to take into account
-# 1. endpoints should be between 0 and 1 (or 0 and 28)
-#    a. model.p0.data.clamp_(0.01, 0.99)
-#       model.p1.data.clamp_(0.01, 0.99)
-#       although you lose the gradient if it goes off
-#    b. alternatively, the function is differentiable everywhere, although
-#       frankly I don't want rendering to affect every pixel, even during training
-#       Note: I tried this, and sometimes the gradient is so weak it doesn't affect loss
-#       I don't think this is enough for it to recover
-#    c. I just don't want endpoints to end up off canvas because then
-#       it messes with length calculations and if the line goes out of bounds
-#       then there is no gradient
-#    I think a happy medium is applying a sigmoid potentially with some alpha < 1
-#    This clamps it and still provides some gradient
-# 2. endpoints must have a length <= some value
-# 3. rendered outputs must be between 0 and 1
-#
-# worst case it goes out of bounds and the AI doesn't use its entire distance
 
 
 if __name__ == "__main__":
     canvas_shape = (28, 28)
 
-    """ sunset target
+    #""" sunset target
     target_canvas = torch.zeros(canvas_shape)
     for y in range(canvas_shape[0]):
         target_canvas[y, :] = y / canvas_shape[0]
-    """
+    #"""
 
-    """ exact line target """
-    target_p0 = torch.tensor([0.1, 0.1])
+    """ exact line target
+    target_p0 = torch.tensor([0.4, 0.1])
     target_p1 = torch.tensor([0.9, 0.9])
-    target_canvas = LineRaster2d(canvas_shape, line_width=4.0)(target_p0, target_p1)
+    target_canvas = PathRaster2d(canvas_shape, line_width=4.0)([target_p0, target_p1])
+    """
 
     cv2.imshow("target_canvas", target_canvas.numpy())
     cv2.waitKey(0)
@@ -109,7 +172,7 @@ if __name__ == "__main__":
     model = OpponentModel(canvas_shape, line_width=2.0)
 
     criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=3.0)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.1) # 1.0 for global, 0.1 for linear
 
     while True:
         # zero the parameter gradients
@@ -123,7 +186,7 @@ if __name__ == "__main__":
         loss.backward()
         optimizer.step()
 
-        print(list(model.parameters()))
+        #print(list(model.parameters()))
         print(f"loss: {loss.item()}")
         cv2.imshow("output_canvas", output_canvas.detach().numpy())
         cv2.waitKey(0)
