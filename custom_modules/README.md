6d52f972dfe0059b476e714428f006c54cf01561
Kyle Sayers <kylesayrs@gmail.com>
2022-12-26 02:22:38 -0500

    Add readme, curve target

---
+++ custom_modules/README.md
@@ -0,0 +1,6 @@
+TODO:
+0. Investigate post backward clamping not working?
+1. Implement random initialization strategy
+2. Implement alias decay strategy
+3. Implement score model
+4. Evaluate which strategy is best
--- custom_modules/demo_optimization.py
+++ custom_modules/demo_optimization.py
@@ -3,6 +3,7 @@ import torch
 import numpy
 
 from OpponentModel import OpponentModel
+from LineGraphic2d import LineGraphic2d
 from CurveGraphic2d import CurveGraphic2d
 
 
@@ -10,9 +11,12 @@ def draw_output_and_target(output_canvas, target_canvas):
     assert output_canvas.shape == target_canvas.shape
     image = numpy.zeros((*output_canvas.shape, 3))
 
-    image[:, :, 0] = output_canvas.detach().numpy()
-    image[:, :, 1] = output_canvas.detach().numpy()
-    image[:, :, 2] = target_canvas.detach().numpy()
+    output = output_canvas.detach().numpy()
+    target = target_canvas.detach().numpy()
+
+    image[:, :, 0] = cv2.bitwise_and(1.0 - output, target)
+    image[:, :, 1] = cv2.bitwise_and(output, target)
+    image[:, :, 2] = cv2.bitwise_and(output, 1.0 - target)
 
     cv2.imshow("output and target", image)
     cv2.waitKey(0)
@@ -27,21 +31,42 @@ if __name__ == "__main__":
         target_canvas[y, :] = y / canvas_shape[0]
     """
 
-    #""" exact line target
-    target_p0 = torch.tensor([0.4, 0.1])
-    target_p1 = torch.tensor([0.9, 0.9])
-    target_canvas = CurveGraphic2d(canvas_shape, width=4.0)([target_p0, target_p1])
+    """ exact line target
+    target_points = [
+        torch.tensor([0.4, 0.1]),
+        torch.tensor([0.9, 0.9]),
+    ]
+    target_canvas = LineGraphic2d(canvas_shape, width=4.0)(target_points)
+    """
+
+    #""" exact line curve
+    target_points = [
+        torch.tensor([0.0, 0.0]), #
+        torch.tensor([0.1, 1.0]),
+        torch.tensor([0.3, 0.8]), #
+        torch.tensor([1.0, 0.6]),
+        torch.tensor([0.8, 0.4]), #
+        torch.tensor([0.2, 0.1]),
+        torch.tensor([0.5, 0.3]), #
+    ]
+    target_canvas = CurveGraphic2d(
+        canvas_shape,
+        num_samples=30,
+        width=5.0,
+        anti_aliasing_factor=0.25,
+    )(target_points)
     #"""
 
     model = OpponentModel(
         canvas_shape=canvas_shape,
-        num_key_points=4,
+        num_key_points=7,
+        num_samples=15,
         width=3.0,
         anti_aliasing_factor=0.25
     )
 
     criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.8, momentum=0.9)
 
     while True:
         # zero the parameter gradients
