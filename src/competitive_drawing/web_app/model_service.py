b3e292466ccba4c70baa3cbfa3556be6653ed65a
Kyle Sayers <kylesayrs@gmail.com>
2023-12-22 15:33:11 -0500

    minor cleanup

--- src/competitive_drawing/web_app/model_service.py
+++ src/competitive_drawing/web_app/model_service.py
@@ -10,14 +10,14 @@ SETTINGS = Settings()
 HEADERS = {"Content-Type": "application/json"}
 
 
-def server_infer(label_pair: Tuple[str, str], canvas_preview_data_url: str) -> List[float]:
+def server_infer(label_pair: Tuple[str, str], image_data_url: str) -> List[float]:
     response = requests.post(
         f"{SETTINGS.model_service_base}/infer",
         headers=HEADERS,
         data=json.dumps({
             "gameConfig": GAME_CONFIG,
             "label_pair": label_pair,
-            "imageDataUrl": canvas_preview_data_url
+            "imageDataUrl": image_data_url
         })
     )
 
