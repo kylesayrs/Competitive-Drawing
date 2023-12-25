886359d5829f1e6015354433dc43288974c2ce3d
Kyle Sayers <kylesayrs@gmail.com>
2023-12-25 16:32:24 -0500

    test_emit_start_game

--- tests/web_app/sockets/test_emit.py
+++ tests/web_app/sockets/test_emit.py
@@ -1,13 +1,14 @@
 import pytest
 import json
 from flask import request
-from flask_socketio import SocketIOTestClient
+from flask_socketio import SocketIOTestClient, join_room
 
 from competitive_drawing.web_app.app import app, socketio
 from competitive_drawing.web_app.game import create_game, GameType, Player
 from competitive_drawing.web_app.sockets import (
     emit_assign_player,
-    emit_start_turn
+    emit_start_turn,
+    emit_start_game
 )
 
 
@@ -21,6 +22,14 @@ def get_client_sid(client: SocketIOTestClient) -> str:
     client.emit("connect")
 
     return client_sid
+
+
+def client_join_room(client: SocketIOTestClient, room_id: str):
+    @socketio.on("test_join")
+    def connect():
+        join_room(room_id)
+
+    client.emit("test_join")
 
 
 @pytest.mark.parametrize(
@@ -48,9 +57,7 @@ def test_emit_assign_player(player_id):
 def test_emit_start_turn(game_type):
     game = create_game(game_type)
     two_player_game = game_type in [GameType.ONLINE]
-
-    # reduce canvas image to make message checking easier
-    game.canvas_image = []
+    game.canvas_image = []  # don't check canvas image
 
     # client_a connect
     client_a = SocketIOTestClient(app, socketio)
@@ -71,7 +78,7 @@ def test_emit_start_turn(game_type):
         target_index=0,
     )
     game.players.append(player_a)
-    client_a.emit("join_room", {"roomId": game.room_id, "playerId": player_a.id})
+    client_join_room(client_a, game.room_id)
 
     # client_b add (directly to avoid side effect emissions)
     if two_player_game:
@@ -83,7 +90,7 @@ def test_emit_start_turn(game_type):
             target_index=1,
         )
         game.players.append(player_b)
-        client_b.emit("join_room", {"roomId": game.room_id, "playerId": player_b.id})
+        client_join_room(client_b, game.room_id)
 
     # emit start turn
     emit_start_turn(game)
@@ -91,7 +98,7 @@ def test_emit_start_turn(game_type):
     # check received
     assert json.dumps(client_a.get_received()) == (
         '[{"name": "start_turn", "args": [{"canvas": [], "turn": "player_a", '
-        f'"target": "{game.label_pair[0]}", "turnsLeft": 10, "modelOutputs": '
+        f'"target": "{player_a.target}", "turnsLeft": 10, "modelOutputs": '
         '[0.0, 0.0]}], "namespace": "/"}]'
     )
 
@@ -99,6 +106,84 @@ def test_emit_start_turn(game_type):
     if two_player_game:
         assert json.dumps(client_b.get_received()) == (
             '[{"name": "start_turn", "args": [{"canvas": [], "turn": "player_a", '
-            f'"target": "{game.label_pair[0]}", "turnsLeft": 10, "modelOutputs": '
+            f'"target": "{player_a.target}", "turnsLeft": 10, "modelOutputs": '
             '[0.0, 0.0]}], "namespace": "/"}]'
+        )
+
+
+@pytest.mark.parametrize(
+    "game_type,to_player_one",
+    [
+        (GameType.LOCAL, False),
+        (GameType.ONLINE, False),
+        (GameType.SINGLE_PLAYER, False)
+    ],
+)
+def test_emit_start_game(game_type, to_player_one):
+    game = create_game(game_type)
+    two_player_game = game_type in [GameType.ONLINE]
+    game.canvas_image = []  # don't check canvas image
+
+    # client_a connect
+    client_a = SocketIOTestClient(app, socketio)
+    client_a.connect()
+    assert client_a.is_connected()
+
+    # client_b connect
+    client_b = SocketIOTestClient(app, socketio)
+    client_b.connect()
+    assert client_b.is_connected()
+
+    # client_a add (directly to avoid side effect emissions)
+    client_a_sid = get_client_sid(client_a)
+    player_a = Player(
+        id="player_a",
+        sid=client_a_sid,
+        target=game.label_pair[0],
+        target_index=0,
+    )
+    game.players.append(player_a)
+    client_join_room(client_a, game.room_id)
+
+    # client_b add (directly to avoid side effect emissions)
+    if two_player_game:
+        client_b_sid = get_client_sid(client_b)
+        player_b = Player(
+            id="player_b",
+            sid=client_b_sid,
+            target=game.label_pair[1],
+            target_index=1,
+        )
+        game.players.append(player_b)
+        client_join_room(client_b, game.room_id)
+
+    # emit start turn
+    emit_start_game(game)
+
+    # check received
+    if not two_player_game:
+        client_a_data = client_a.get_received()
+        del client_a_data[0]["args"][0]["onnxUrl"]  # don't check onnxUrl
+        assert json.dumps(client_a_data) == (
+            '[{"name": "start_game", "args": [{"canvas": [], "targets": {"player_a": '
+            f'"{player_a.target}"}}, "targetIndices": {{"player_a": 0}}, '
+            '"totalNumTurns": 10}], "namespace": "/"}]'
+        )
+    else:
+        client_a_data = client_a.get_received()
+        del client_a_data[0]["args"][0]["onnxUrl"]  # don't check onnxUrl
+        assert json.dumps(client_a_data) == (
+            '[{"name": "start_game", "args": [{"canvas": [], "targets": {"player_a": '
+            f'"{player_a.target}", "player_b": "{player_b.target}"}}, '
+            '"targetIndices": {"player_a": 0, "player_b": 1}, "totalNumTurns": 10}], '
+            '"namespace": "/"}]'
+        )
+            
+        client_b_data = client_b.get_received()
+        del client_b_data[0]["args"][0]["onnxUrl"]  # don't check onnxUrl
+        assert json.dumps(client_b_data) == (
+            '[{"name": "start_game", "args": [{"canvas": [], "targets": {"player_a": '
+            f'"{player_a.target}", "player_b": "{player_b.target}"}}, '
+            '"targetIndices": {"player_a": 0, "player_b": 1}, "totalNumTurns": 10}], '
+            '"namespace": "/"}]'
         )
